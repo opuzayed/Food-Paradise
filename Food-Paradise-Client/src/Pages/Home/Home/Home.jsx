6af0c22c19fc5274a6a5b0d781d2347764737e35
zayed opu <zayedopu4933@gmail.com>
2024-08-11 22:22:55 +0600

    Banner page created

--- Food-Paradise-Client/src/Pages/Home/Home/Home.jsx
+++ Food-Paradise-Client/src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <p>this is home</p>
+            <Banner></Banner>
         </div>
     );
 };
