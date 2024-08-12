28bd8cc9e88794621a87468fced3b2d59666ed82
zayed opu <zayedopu4933@gmail.com>
2024-08-12 08:44:21 +0600

    swiper image done

--- Food-Paradise-Client/src/Pages/Home/Category/Category.jsx
+++ Food-Paradise-Client/src/Pages/Home/Category/Category.jsx
@@ -1,10 +1,39 @@
-
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import slide1 from '../../../assets/home/slide1.jpg';
+import slide2 from '../../../assets/home/slide2.jpg';
+import slide3 from '../../../assets/home/slide3.jpg';
+import slide4 from '../../../assets/home/slide4.jpg';
+import slide5 from '../../../assets/home/slide5.jpg';
+import slide6 from '../../../assets/home/slide6.jpg';
+import slide7 from '../../../assets/home/slide7.jpg';
+import slide8 from '../../../assets/home/slide8.jpg';
 const Category = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-10"
+            >
+                <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide6} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide7} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slide8} alt="" /></SwiperSlide>
+
+            </Swiper>
+        </>
     );
 };
 
