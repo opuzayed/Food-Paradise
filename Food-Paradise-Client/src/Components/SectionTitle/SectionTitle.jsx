06fb69dcb82b7f003e5c7f13b8d86b8cf5a90c4f
zayed opu <zayedopu4933@gmail.com>
2024-08-12 10:50:58 +0600

    border applied top and bottom

--- Food-Paradise-Client/src/Components/SectionTitle/SectionTitle.jsx
+++ Food-Paradise-Client/src/Components/SectionTitle/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({subHeading, heading}) => {
     return (
         <div className='text-center'>
             <p className='text-cyan-500'>---{subHeading}---</p>
-            <h3 className='text-3xl uppercase'>{heading}</h3>
+            <h3 className='text-3xl uppercase border-y-2'>{heading}</h3>
         </div>
     );
 };
