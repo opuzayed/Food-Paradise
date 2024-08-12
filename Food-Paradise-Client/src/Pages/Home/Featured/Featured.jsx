00f102eb5a28a4e5fd0352fa7868a6632cbbafb4
zayed opu <zayedopu4933@gmail.com>
2024-08-12 21:27:29 +0600

    btn decorated

--- Food-Paradise-Client/src/Pages/Home/Featured/Featured.jsx
+++ Food-Paradise-Client/src/Pages/Home/Featured/Featured.jsx
@@ -4,17 +4,17 @@ import ('./featured.css');
 
 const Featured = () => {
     return (
-        <section className="featured-item text-white pt-6 py-24">
+        <section className="featured-item text-white pt-32">
             <SectionTitle subHeading={'should try'} heading={"Chef Recommended"}></SectionTitle>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center pb-20 px-40 pt-14">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
                 <div className="md:ml-10">
                     <p>Aug 20, 2024</p>
                     <p className="uppercase">Where Can I get some</p>
-                    <p>Fuska, also known as Phuchka or Puchka, is a beloved street food originating from the Indian subcontinent, particularly popular in Bangladesh and Kolkata, India.It consists of crispy, hollow puris filled with a spicy and tangy mixture of mashed potatoes, chickpeas, and tamarind water.Fuska is enjoyed as a snack and is often sold by street vendors.It’s known for its unique combination of textures and flavors, making it a favorite among locals and visitors alike.</p>
-                    <button className="btn btn-outline uppercase">Order Now</button>
+                    <p>Fuska, also known as Phuchka or Puchka, is a beloved street food originating from the Indian subcontinent, particularly popular in Bangladesh and Kolkata, India.It consists of crispy, hollow puris filled with a spicy and tangy mixture of mashed potatoes, chickpeas, and tamarind water.Fuska is enjoyed as a snack and is often sold by street vendors.</p>
+                    <button className="btn btn-outline btn-info uppercase border-0 border-b-4 mt-2">Order Now</button>
                 </div>
             </div>
         </section>
@@ -22,3 +22,4 @@ const Featured = () => {
 };
 
 export default Featured;
+// pb-10 pt-12
