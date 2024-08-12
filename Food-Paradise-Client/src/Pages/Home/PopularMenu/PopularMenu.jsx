7d8aa02e43447cc927a9a025d3a8a4cdbe919fc3
zayed opu <zayedopu4933@gmail.com>
2024-08-12 12:01:18 +0600

    popular item loaded

--- Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(()=>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            const popularItems = data.filter(item => item.category === 'popular')
+            setMenu(popularItems)
+        })
+    }, [])
     return (
         <section>
-            
+            <SectionTitle subHeading={'check it out'} heading={'From Our Menu'}></SectionTitle>
         </section>
     );
 };
