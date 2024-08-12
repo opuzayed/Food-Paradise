9785fa773a70f9c1f4c97ba61cf46720540752e7
zayed opu <zayedopu4933@gmail.com>
2024-08-12 12:27:58 +0600

    flex apply

--- Food-Paradise-Client/src/Pages/Shared/MenuItem/MenuItem.jsx
+++ Food-Paradise-Client/src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const MenuItem = ({item}) => {
     const {name, recipe, image, price} = item;
     return (
-        <div>
+        <div className='flex space-x-4'>
             <img src={image} className='w-[100px]' alt="" />
             <div>
                 <h3 className='uppercase'>{name}--------------------------</h3>
                 <p>{recipe}</p>
             </div>
-            <p>{price}</p>
+            <p className='text-cyan-500'>${price}</p>
         </div>
     );
 };
