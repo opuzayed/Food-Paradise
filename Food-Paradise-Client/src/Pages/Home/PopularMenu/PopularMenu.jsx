95f1b9307c05ffa2d823e1ca935e8d1c36824df2
zayed opu <zayedopu4933@gmail.com>
2024-08-13 00:01:02 +0600

    view full menu btn set centered

--- Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -13,13 +13,14 @@ const PopularMenu = () => {
         })
     }, [])
     return (
-        <section className='mb-20'>
+        <section className='mb-20 flex flex-col items-center'>
             <SectionTitle subHeading={'check it out'} heading={'From Our Menu'}></SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
                     menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
+            <button className="btn btn-outline btn-info uppercase border-0 border-b-4 mt-6">View Full Menu</button>
         </section>
     );
 };
