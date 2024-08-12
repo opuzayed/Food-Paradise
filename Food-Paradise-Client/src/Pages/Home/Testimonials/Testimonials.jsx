658685608dfb89c6c435be3e265b4a13d77b179e
zayed opu <zayedopu4933@gmail.com>
2024-08-12 23:30:44 +0600

    rating added into testimonial section

--- Food-Paradise-Client/src/Pages/Home/Testimonials/Testimonials.jsx
+++ Food-Paradise-Client/src/Pages/Home/Testimonials/Testimonials.jsx
@@ -4,6 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
 
 const Testimonials = () => {
 
@@ -11,22 +13,27 @@ const Testimonials = () => {
 
     useEffect(() => {
         fetch("reviews.json")
-        .then(res => res.json())
-        .then(data => setReviews(data))
+            .then(res => res.json())
+            .then(data => setReviews(data))
     }, [])
 
     return (
         <section className='mb-20'>
             <SectionTitle subHeading={"What Our Client Say"} heading={"Testimonials"}></SectionTitle>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                
+
                 {
                     reviews.map(review => <SwiperSlide key={review._id}>
-                       <div className='m-20 pt-6 flex flex-col items-center'>
-                        <p>{review.details}</p>
-                        <h3 className="text-2xl text-orange-300">{review.name}</h3>
-                       </div>
-                        </SwiperSlide>)
+                        <div className='m-16 pt-2 flex flex-col items-center'>
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <p>{review.details}</p>
+                            <h3 className="text-2xl text-orange-300">{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
                 }
             </Swiper>
         </section>
