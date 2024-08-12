ec23eee437a39b02a2aea98e6a05d7aa54d9c2dd
zayed opu <zayedopu4933@gmail.com>
2024-08-12 11:00:22 +0600

    section title done

--- Food-Paradise-Client/src/Components/SectionTitle/SectionTitle.jsx
+++ Food-Paradise-Client/src/Components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({subHeading, heading}) => {
     return (
-        <div className='text-center'>
-            <p className='text-cyan-500'>---{subHeading}---</p>
-            <h3 className='text-3xl uppercase border-y-2'>{heading}</h3>
+        <div className='md:w-3/12 mx-auto text-center my-8'>
+            <p className='text-cyan-500 mb-2'>---{subHeading}---</p>
+            <h3 className='text-3xl uppercase border-y-2 py-2'>{heading}</h3>
         </div>
     );
 };
