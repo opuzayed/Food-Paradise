0340a68ead4adea725ed53c9a7aa48de9152da14
zayed opu <zayedopu4933@gmail.com>
2024-08-12 23:15:59 +0600

    reviews part done

--- Food-Paradise-Client/src/Pages/Home/Testimonials/Testimonials.jsx
+++ Food-Paradise-Client/src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 const Testimonials = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("reviews.json")
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    }, [])
+
     return (
-        <section>
+        <section className='mb-20'>
             <SectionTitle subHeading={"What Our Client Say"} heading={"Testimonials"}></SectionTitle>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                
+                {
+                    reviews.map(review => <SwiperSlide key={review._id}>
+                       <div className='m-20 pt-6 flex flex-col items-center'>
+                        <p>{review.details}</p>
+                        <h3 className="text-2xl text-orange-300">{review.name}</h3>
+                       </div>
+                        </SwiperSlide>)
+                }
+            </Swiper>
         </section>
     );
 };
