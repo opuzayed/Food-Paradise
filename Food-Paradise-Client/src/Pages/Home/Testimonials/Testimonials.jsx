0a4a7231ee27260db2d21832bd196d123889f85a
zayed opu <zayedopu4933@gmail.com>
2024-08-12 21:39:18 +0600

    testimonials section title set

---
+++ Food-Paradise-Client/src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+
+const Testimonials = () => {
+    return (
+        <section>
+            <SectionTitle subHeading={"What Our Client Say"} heading={"Testimonials"}></SectionTitle>
+        </section>
+    );
+};
+
+export default Testimonials;
