72be12e877d393400e02c863b7545b7acd1c6d2a
zayed opu <zayedopu4933@gmail.com>
2024-08-12 12:23:28 +0600

    set fixed width for image

--- Food-Paradise-Client/src/Pages/Shared/MenuItem/MenuItem.jsx
+++ Food-Paradise-Client/src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -4,7 +4,12 @@ const MenuItem = ({item}) => {
     const {name, recipe, image, price} = item;
     return (
         <div>
-            
+            <img src={image} className='w-[100px]' alt="" />
+            <div>
+                <h3 className='uppercase'>{name}--------------------------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p>{price}</p>
         </div>
     );
 };
