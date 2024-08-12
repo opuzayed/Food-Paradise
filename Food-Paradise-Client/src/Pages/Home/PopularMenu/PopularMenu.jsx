e43b4907a1e61ffe21cfbcc99837ef614495634c
zayed opu <zayedopu4933@gmail.com>
2024-08-12 12:35:31 +0600

    apply borderRadius for shape the image

--- Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -15,7 +15,7 @@ const PopularMenu = () => {
     return (
         <section>
             <SectionTitle subHeading={'check it out'} heading={'From Our Menu'}></SectionTitle>
-            <div>
+            <div className='grid md:grid-cols-2 gap-5'>
                 {
                     menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
--- Food-Paradise-Client/src/Pages/Shared/MenuItem/MenuItem.jsx
+++ Food-Paradise-Client/src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -4,9 +4,9 @@ const MenuItem = ({item}) => {
     const {name, recipe, image, price} = item;
     return (
         <div className='flex space-x-4'>
-            <img src={image} className='w-[100px]' alt="" />
+            <img style={{borderRadius:'0px 200px 200px 200px'}} src={image} className ='w-[100px]' alt="" />
             <div>
-                <h3 className='uppercase'>{name}--------------------------</h3>
+                <h3 className='uppercase'>{name}-----------------------</h3>
                 <p>{recipe}</p>
             </div>
             <p className='text-cyan-500'>${price}</p>
