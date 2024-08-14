fc9bb9126fcef0075ca56ee26f9740f7bf416de8
zayed opu <zayedopu4933@gmail.com>
2024-08-14 10:10:24 +0600

    popular item loaded from custom hook

--- Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ Food-Paradise-Client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import MenuItem from '../../Shared/MenuItem/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular')
-            setMenu(popularItems)
-        })
-    }, [])
+    const [menu] = useMenu();
+    const popularItem = menu.filter(item => item.category === 'popular');
+    // const [menu, setMenu] = useState([]);
+    // useEffect(()=>{
+    //     fetch('menu.json')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const popularItems = data.filter(item => item.category === 'popular')
+    //         setMenu(popularItems)
+    //     })
+    // }, [])
     return (
         <section className='mb-20 flex flex-col items-center'>
             <SectionTitle subHeading={'check it out'} heading={'From Our Menu'}></SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
-                    menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                    popularItem.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
             <button className="btn btn-outline btn-info uppercase border-0 border-b-4 mt-6">View Full Menu</button>
