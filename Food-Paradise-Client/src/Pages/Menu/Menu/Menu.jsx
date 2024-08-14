68d83bfc491c1c1cdd89c618db8f4efd43778ac9
zayed opu <zayedopu4933@gmail.com>
2024-08-14 08:16:31 +0600

    menu backgroundImage set

--- Food-Paradise-Client/src/Pages/Menu/Menu/Menu.jsx
+++ Food-Paradise-Client/src/Pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Helmet} from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/menubg.jpg';
 const Menu = () => {
     return (
         <div>
            <Helmet>
                 <title>Food Paradise | Menu</title>
             </Helmet>
+            <Cover img={menuImg}></Cover>
         </div>
     );
 };
