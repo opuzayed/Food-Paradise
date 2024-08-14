0f7246a2626103c7f6a447eff19814bda586dfe6
zayed opu <zayedopu4933@gmail.com>
2024-08-14 10:54:29 +0600

    filter category item

--- Food-Paradise-Client/src/Pages/Menu/Menu/Menu.jsx
+++ Food-Paradise-Client/src/Pages/Menu/Menu/Menu.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { Helmet} from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/imagebg.png';
+import useMenu from '../../../hooks/useMenu';
 
 const Menu = () => {
+
+    const [menu] = useMenu();
+
+    const saladItems = menu.filter(item => item.category === 'salad');
+    const soupItems = menu.filte(item => item.category === 'soup');
+    const dessertItems = menu.filter(item => item.category === 'dessert');
+    const pizzaItems = menu.filter(item => item.category === 'pizza');
+    const offeredItems = menu.filter(item => item.category === 'offered');
+
     return (
         <div>
            <Helmet>
