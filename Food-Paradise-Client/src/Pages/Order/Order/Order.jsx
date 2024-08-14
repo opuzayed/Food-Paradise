6bc81ee2743287efff51105878189e0df701885d
zayed opu <zayedopu4933@gmail.com>
2024-08-14 20:08:23 +0600

    order cover image set also set title

--- Food-Paradise-Client/src/Pages/Order/Order/Order.jsx
+++ Food-Paradise-Client/src/Pages/Order/Order/Order.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import orderImg from '../../../assets/shop/neworderbg.jpg';
+import Cover from '../../Shared/Cover/Cover';
 
 const Order = () => {
     return (
         <div>
-            
+            <Cover img={orderImg} title={"Order Food"} shortDes={"Order Your Favourite Food, which is available right now, also enjoy discount for every item."}></Cover>
         </div>
     );
 };
