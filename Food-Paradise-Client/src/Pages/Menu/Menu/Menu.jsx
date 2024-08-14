e41e54ef98fa95db8c5c27ec992afb98607bd7c6
zayed opu <zayedopu4933@gmail.com>
2024-08-14 11:00:17 +0600

    section title added

--- Food-Paradise-Client/src/Pages/Menu/Menu/Menu.jsx
+++ Food-Paradise-Client/src/Pages/Menu/Menu/Menu.jsx
@@ -3,13 +3,14 @@ import { Helmet} from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/imagebg.png';
 import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 
 const Menu = () => {
 
     const [menu] = useMenu();
 
     const saladItems = menu.filter(item => item.category === 'salad');
-    const soupItems = menu.filte(item => item.category === 'soup');
+    const soupItems = menu.filter(item => item.category === 'soup');
     const dessertItems = menu.filter(item => item.category === 'dessert');
     const pizzaItems = menu.filter(item => item.category === 'pizza');
     const offeredItems = menu.filter(item => item.category === 'offered');
@@ -20,7 +21,7 @@ const Menu = () => {
                 <title>Food Paradise | Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"Our Menu"}></Cover>
-            
+            <SectionTitle subHeading={"Don't miss"} heading={"Today's Offer"}></SectionTitle>
         </div>
     );
 };
