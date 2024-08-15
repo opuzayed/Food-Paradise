85af4c14db787b2b5dee43c87b8410ef7a863b19
zayed opu <zayedopu4933@gmail.com>
2024-08-15 21:47:43 +0600

    set db user and pass

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://foodUser:<password>@cluster0.cdjii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdjii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
