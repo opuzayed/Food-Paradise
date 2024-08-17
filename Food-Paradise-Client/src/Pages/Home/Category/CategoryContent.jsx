b2479915f8af12a3a072eb8801d5b84d9830bd14
zayed opu <zayedopu4933@gmail.com>
2024-08-17 22:25:00 +0600

    partially changes few items

--- Food-Paradise-Client/src/Pages/Home/Category/CategoryContent.jsx
+++ Food-Paradise-Client/src/Pages/Home/Category/CategoryContent.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+
 
 const SliderContent = ({img, title}) => {
     return (
         <div className='relative'>
-            <img src={img} alt="" />
-            <span className='absolute bottom-0 left-0 w-full py-5 text-3xl uppercase text-center text-white font-bold'>{title}</span>
+            <img src={img} alt="image" />
+            <h4 className='absolute text-3xl bottom-0 left-0 py-5 uppercase w-full text-center text-white font-bold'>{title}</h4>
         </div>
     );
 };
