6f9faeb5b4ef62366aa4b08d95b7ff2f1cf626c6
zayed opu <zayedopu4933@gmail.com>
2024-08-21 16:53:31 +0600

    login button style applied

--- Food-Paradise-Client/src/Pages/Login/Login.jsx
+++ Food-Paradise-Client/src/Pages/Login/Login.jsx
@@ -1,53 +1,52 @@
 const Login = () => {
-    return (
-      <div className="hero min-h-screen flex items-center justify-center">
-        <div className="hero-content flex flex-col items-center text-center w-full max-w-md md:shadow-2xl md:shadow-slate-700">
-          <div>
-            <h1 className="text-5xl font-bold">Login Here</h1>
-            {/* <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-              a id nisi.
-            </p> */}
-          </div>
-          <div className="card w-full">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
-          </div>
+  return (
+    <div className="hero min-h-screen flex items-center justify-center">
+      <div className="hero-content flex flex-col items-center text-center w-full max-w-md md:shadow-2xl md:shadow-slate-700">
+        <div>
+          <h1 className="text-5xl font-bold">Login Here</h1>
+        </div>
+        <div className="card w-full">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+
+            <div className="form-control mt-6 w-full">
+              <input
+                type="submit"
+                value="Login"
+                className="bg-gradient-to-r from-blue-500 to-green-500 hover:from-blue-700 hover:to-green-700 text-white font-bold text-3xl h-14 rounded-md w-full cursor-pointer"
+              />
+            </div>
+          </form>
         </div>
       </div>
-    );
-  };
-  
-  export default Login;
-  
+    </div>
+  );
+};
+
+export default Login;
