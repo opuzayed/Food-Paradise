cfc6eeedbd339b1fb21c63039a3314a0974988bc
zayed opu <zayedopu4933@gmail.com>
2024-08-21 12:37:47 +0600

    apply sadow on text and input field

--- Food-Paradise-Client/src/Pages/Login/Login.jsx
+++ Food-Paradise-Client/src/Pages/Login/Login.jsx
@@ -1,11 +1,53 @@
-
-
 const Login = () => {
     return (
-        <div>
-            <h2>please Login</h2>
+      <div className="hero min-h-screen flex items-center justify-center">
+        <div className="hero-content flex flex-col items-center text-center w-full max-w-md shadow-2xl shadow-slate-700">
+          <div>
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+              a id nisi.
+            </p>
+          </div>
+          <div className="card w-full">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     );
-};
-
-export default Login;
+  };
+  
+  export default Login;
+  
