d3b2523299e06666e179089838c6a6b512e73ac6
zayed opu <zayedopu4933@gmail.com>
2024-08-26 20:05:12 +0600

    unsubscribe function implemented

--- Food-Paradise-Client/src/Providers/AuthProvider.jsx
+++ Food-Paradise-Client/src/Providers/AuthProvider.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
-import { getAuth } from "firebase/auth";
-import {app} from "../Firebase/firebase.config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -9,16 +9,25 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+   useEffect(()=> {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      console.log("current user", currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    }
+   }, [])
+
   const authInfo = {
     user,
-    loading
+    loading,
   };
 
   return (
-    <AuthContext.Provider value={authInfo}>
-      {children} 
-    
-    </AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
