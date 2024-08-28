a72c90c7a69e6e28c3e101bdbd29958c12a876d9
zayed opu <zayedopu4933@gmail.com>
2024-08-28 16:26:43 +0600

    navigate from private route

--- Food-Paradise-Client/src/Routes/PrivateRoute.jsx
+++ Food-Paradise-Client/src/Routes/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
 
     if(loading){
         return <span className="loading loading-ring loading-lg"></span>
@@ -12,7 +13,7 @@ const PrivateRoute = ({children}) => {
     if(user){
         return children;
     }
-    return <Navigate to="/login"></Navigate>
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
 };
 
 export default PrivateRoute;
