7e0014363f5d8fe231f5eb18a1569f8183cee501
zayed opu <zayedopu4933@gmail.com>
2024-09-03 15:10:53 +0600

    get cart api generated

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -42,6 +42,11 @@ async function run() {
     });
 
     // cart collection
+    app.get('/carts', async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    })
+    
     app.post('/carts', async(req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
