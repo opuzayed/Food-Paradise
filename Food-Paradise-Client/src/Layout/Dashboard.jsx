52a97c5d26f7610f6912d798e3b617e7a2f02ff7
zayed opu <zayedopu4933@gmail.com>
2024-09-04 22:37:34 +0600

    apply style on dashboard layout

--- Food-Paradise-Client/src/Layout/Dashboard.jsx
+++ Food-Paradise-Client/src/Layout/Dashboard.jsx
@@ -4,12 +4,12 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div className="flex">
+        <div className="flex text-black">
             <div className="w-64 min-h-screen bg-orange-200">
-                <h5 className="mt-10 p-4"><span className="font-bold">FOOD PARADISE</span> <br /><span className="tracking-[0.2em]">RESTAURANT</span></h5>
+                <h5 className="mt-4 p-4"><span className="font-bold">FOOD PARADISE</span> <br /><span className="tracking-[0.2em] font-medium">RESTAURANT</span></h5>
                
-            <ul className="menu p-4">
-                <li><NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart>MY CART</NavLink></li>
+            <ul className="p-4 mt-5 font-medium">
+                <li><NavLink to='/dashboard/cart' className="flex items-center"><FaShoppingCart className="mr-2"></FaShoppingCart>MY CART</NavLink></li>
             </ul>
             </div>
             <div className="flex-1">
