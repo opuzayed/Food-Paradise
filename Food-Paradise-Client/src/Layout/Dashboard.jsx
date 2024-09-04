c61615de927bd944147298e9ba79c29477a00fb1
zayed opu <zayedopu4933@gmail.com>
2024-09-04 20:16:19 +0600

    dashboard layout style set

--- Food-Paradise-Client/src/Layout/Dashboard.jsx
+++ Food-Paradise-Client/src/Layout/Dashboard.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom";
 
 
 const Dashboard = () => {
     return (
-        <div>
-            
+        <div className="flex">
+            <div className="w-64 min-h-screen bg-rose-300">
+            <ul>
+                <li><NavLink to='/dashboard/cart'>My Cart</NavLink></li>
+            </ul>
+            </div>
+            <div className="flex-1">
+                <outlet></outlet>
+            </div>
         </div>
     );
 };
