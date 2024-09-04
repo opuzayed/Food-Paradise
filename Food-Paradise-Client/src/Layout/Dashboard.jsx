999f0c5e6d3b2439407376975fdc175b17cb3fc3
zayed opu <zayedopu4933@gmail.com>
2024-09-04 23:57:05 +0600

    dashboard layout structure leftside part done

--- Food-Paradise-Client/src/Layout/Dashboard.jsx
+++ Food-Paradise-Client/src/Layout/Dashboard.jsx
@@ -1,4 +1,5 @@
-import { FaShoppingCart } from "react-icons/fa";
+import { FaCalendar, FaClipboardList, FaComment, FaHome, FaShoppingCart, FaStar } from "react-icons/fa";
+import { FaPaypal } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
 
 
@@ -7,9 +8,19 @@ const Dashboard = () => {
         <div className="flex text-black">
             <div className="w-64 min-h-screen bg-orange-200">
                 <h5 className="mt-4 p-4"><span className="font-bold">FOOD PARADISE</span> <br /><span className="tracking-[0.2em] font-medium">RESTAURANT</span></h5>
+            <ul className="p-4 mt-5 font-medium space-y-4">
+                
+                <li><NavLink to='/dashboard/userHome' className="flex items-center"><FaHome className="mr-2"></FaHome>USER HOME</NavLink></li>
+
+                <li><NavLink to='/dashboard/reservation' className="flex items-center"><FaCalendar className="mr-2"></FaCalendar>RESERVATION</NavLink></li>
+
+                <li><NavLink to='/dashboard/paymentHistory' className="flex items-center"><FaPaypal className="mr-2"></FaPaypal>PAYMENT HISTORY</NavLink></li>
+
+                <li><NavLink to='/dashboard/cart' className="flex items-center text-white"><FaShoppingCart className="mr-2"></FaShoppingCart>MY CART</NavLink></li>
                
-            <ul className="p-4 mt-5 font-medium">
-                <li><NavLink to='/dashboard/cart' className="flex items-center"><FaShoppingCart className="mr-2"></FaShoppingCart>MY CART</NavLink></li>
+                <li><NavLink to='/dashboard/review' className="flex items-center "><FaComment className="mr-2"></FaComment>ADD REVIEW</NavLink></li>
+
+                <li><NavLink to='/dashboard/booking' className="flex items-center "><FaClipboardList className="mr-2"></FaClipboardList>MY BOOKINGS</NavLink></li>
             </ul>
             </div>
             <div className="flex-1">
