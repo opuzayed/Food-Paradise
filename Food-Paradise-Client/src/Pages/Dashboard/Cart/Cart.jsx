68823353f5f639702a51c12fa61065bd809dd91d
zayed opu <zayedopu4933@gmail.com>
2024-09-05 15:20:20 +0600

    table added in dashboard layout

--- Food-Paradise-Client/src/Pages/Dashboard/Cart/Cart.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,20 +1,72 @@
 import useCart from "../../../hooks/useCart";
 
-
 const Cart = () => {
-    const [cart] = useCart();
-    const totalPrice = cart.reduce((total, item) => total + item.price ,0);
-    return (
-        <div>
-            <div className="flex justify-around">
-            <h2 className="text-3xl font-medium">
-               ITEMS: {cart.length}
-            </h2>
-            <h2 className="text-3xl font-medium">TOTAL PRICE: {totalPrice}</h2>
-            <button className="btn btn-primary">PAY</button>
-            </div>
-        </div>
-    );
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  return (
+    <div>
+      <div className="flex justify-evenly">
+        <h2 className="text-3xl font-medium">ITEMS: {cart.length}</h2>
+        <h2 className="text-3xl font-medium">TOTAL PRICE: {totalPrice}</h2>
+        <button className="btn btn-primary">PAY</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                #
+              </th>
+              <th>IMAGE</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                cart.map(item => <tr key= {item._id}>
+                    <th>
+                      <label>
+                        <input type="checkbox" className="checkbox" />
+                      </label>
+                    </th>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img
+                              src={item.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">Hart Hagerty</div>
+                          <div className="text-sm opacity-50">United States</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      Zemlak, Daniel and Leannon
+                      <br />
+                      <span className="badge badge-ghost badge-sm">
+                        Desktop Support Technician
+                      </span>
+                    </td>
+                    <td>Purple</td>
+                    <th>
+                      <button className="btn btn-ghost btn-xs">details</button>
+                    </th>
+                  </tr>)
+            }
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
