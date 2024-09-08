3a19203798dfdb799349b3962736b1edc1f7825f
zayed opu <zayedopu4933@gmail.com>
2024-09-08 14:44:56 +0600

    social login component make

---
+++ Food-Paradise-Client/src/Components/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,16 @@
+import { FaGoogle } from "react-icons/fa6";
+
+const SocialLogin = () => {
+  return (
+    <div>
+      <div>
+        <button className="btn">
+          <FaGoogle></FaGoogle>
+          Sign up with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
