9e89c277f044aab6f48ba558c70be147a0c9d066
zayed opu <zayedopu4933@gmail.com>
2024-09-08 15:20:17 +0600

    google signup complete

--- Food-Paradise-Client/src/Components/SocialLogin/SocialLogin.jsx
+++ Food-Paradise-Client/src/Components/SocialLogin/SocialLogin.jsx
@@ -1,11 +1,20 @@
 import { FaGoogle } from "react-icons/fa6";
+import useAuth from "../../hooks/useAuth";
 
 const SocialLogin = () => {
+  const {googleSignIn} = useAuth();
+
+  const handleGoogleSignIn = () => {
+googleSignIn()
+.then(result => {
+  console.log(result.user);
+})
+  }
   return (
     <div>
         <div className="divider">OR</div>
       <div>
-        <button className="btn w-full btn-outline btn-success">
+        <button onClick={handleGoogleSignIn} className="btn w-full btn-outline btn-success">
           <FaGoogle></FaGoogle>
           Sign up with Google
         </button>
