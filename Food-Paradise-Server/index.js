eafa279b8a2eddc02238035ab67ba13d3f08fac4
zayed opu <zayedopu4933@gmail.com>
2024-09-08 16:37:25 +0600

    check user alredy exist in db

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -32,6 +32,12 @@ async function run() {
     //users api 
     app.post("/users", async(req, res) => {
       const user = req.body;
+      const query = {email : user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser)
+      {
+        return res.send({message : 'User Already Exist', insertedId : null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
