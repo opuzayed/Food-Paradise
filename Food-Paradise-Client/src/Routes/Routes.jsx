a18d69a1ab631bf55d21a85f304a2748e8fec020
zayed opu <zayedopu4933@gmail.com>
2024-09-10 07:52:22 +0600

    all users routes set

--- Food-Paradise-Client/src/Routes/Routes.jsx
+++ Food-Paradise-Client/src/Routes/Routes.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../Pages/Dashboard/Cart/Cart";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 
 
 
@@ -52,6 +53,12 @@ import Cart from "../Pages/Dashboard/Cart/Cart";
         {
           path:'cart',
           element:<Cart></Cart>
+        },
+
+        //admin route
+        {
+          path:'/users',
+          element: <AllUsers></AllUsers>
         }
       ]
     }
