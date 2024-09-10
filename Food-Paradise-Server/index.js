329a7f114e0b3a9cafa3b12c6182f1df1440bb56
zayed opu <zayedopu4933@gmail.com>
2024-09-10 11:29:45 +0600

    user delete api created

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -48,6 +48,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) =>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const updateDoc = {
+        $set : {
+          role : 'admin'
+        }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
