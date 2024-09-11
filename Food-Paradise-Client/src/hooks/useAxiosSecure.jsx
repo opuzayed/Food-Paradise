3fd43ea829add5f4eb8eaeefc3687c884d40ea70
zayed opu <zayedopu4933@gmail.com>
2024-09-11 12:48:25 +0600

    request interceptors to add authorization header for every secure call to the api

--- Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
+++ Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
@@ -4,9 +4,11 @@ const axiosSecure = axios.create({
     baseURL : 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    //request interceptors to add authorization header for every secure call to the api
     axiosSecure.interceptors.request.use(function(config){
         const token = localStorage.getItem('access-token');
         console.log('request stopped by interceptors', token);
+        config.headers.authorization = `Bearer ${token}`;
         return config;
     }, (error) => {
         return Promise.reject(error);
