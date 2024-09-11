34914e3c7b1be57a78161e2b43616659ca80d02b
zayed opu <zayedopu4933@gmail.com>
2024-09-11 21:48:48 +0600

    verify admin api created

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -54,6 +54,18 @@ async function run() {
       });
     }
 
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email : email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin)
+      {
+        return res.status(403).send({message : 'forbidden access'});
+      }
+      next();
+    }
+
     //users api 
     
     app.get("/users", verifyToken, async (req, res) => {
