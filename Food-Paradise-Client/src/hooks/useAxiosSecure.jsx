6c72a5e50a6a2d4c7f3be7bb7004ab626b3fab19
zayed opu <zayedopu4933@gmail.com>
2024-09-11 19:41:36 +0600

    unauthorizes access forcefuly logout

--- Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
+++ Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL : 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
     //request interceptors to add authorization header for every secure call to the api
+    const navigate = useNavigate();
+    const {logOut} = useAuth();
+
     axiosSecure.interceptors.request.use(function(config){
         const token = localStorage.getItem('access-token');
         console.log('request stopped by interceptors', token);
@@ -17,9 +22,13 @@ const useAxiosSecure = () => {
     // interceptor 401 and 403 status
     axiosSecure.interceptors.response.use(function (response){
         return response;
-    }, (error) =>{
+    }, async (error) =>{
         const status = error.response.status;
         console.log('status error in the interceptors', status);
+        if(status === 401 || status === 403){
+            await logOut();
+            navigate('/login');
+        }
         return Promise.reject(error);
     });
     return axiosSecure;
