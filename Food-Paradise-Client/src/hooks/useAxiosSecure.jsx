bef01b1ca02604be61d243ca1570629e0b9a53c9
zayed opu <zayedopu4933@gmail.com>
2024-09-11 12:41:01 +0600

    token get from local storage

--- Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
+++ Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,13 @@ const axiosSecure = axios.create({
     baseURL : 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token');
+        console.log('request stopped by interceptors', token);
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    });
     return axiosSecure;
 };
 
