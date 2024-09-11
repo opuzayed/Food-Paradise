c024d27453b094e113f14248f9f8b32f67066c81
zayed opu <zayedopu4933@gmail.com>
2024-09-11 20:20:02 +0600

    useAdmin hook created

--- Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
+++ Food-Paradise-Client/src/hooks/useAxiosSecure.jsx
@@ -25,6 +25,7 @@ const useAxiosSecure = () => {
     }, async (error) =>{
         const status = error.response.status;
         console.log('status error in the interceptors', status);
+        //401 and 403 logout the user and navigate to login page
         if(status === 401 || status === 403){
             await logOut();
             navigate('/login');
