5569e34ce1ef8b8785e52d92c470cd8271e0f8c3
zayed opu <zayedopu4933@gmail.com>
2024-09-12 20:00:51 +0600

    addItems component make

--- Food-Paradise-Client/src/Routes/AdminRoutes.jsx
+++ Food-Paradise-Client/src/Routes/AdminRoutes.jsx
@@ -3,9 +3,9 @@ import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
 
-const AdminRoutes = () => {
-    //vai boshaise const {user, loading} = useAuth();
-    
+const AdminRoutes = ({children}) => {
+    //vai boshaise const [user, loading] = useAuth();
+
     const {user, loading} = useAuth(); 
     const [isAdmin, isAdminLoading] = useAdmin();
     const location = useLocation();
@@ -14,11 +14,13 @@ const AdminRoutes = () => {
     {
         return <span className="loading loading-ring loading-lg"></span>
     }
+
     if(user && isAdmin)
     {
         return children;
     }
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
 
 };
 
