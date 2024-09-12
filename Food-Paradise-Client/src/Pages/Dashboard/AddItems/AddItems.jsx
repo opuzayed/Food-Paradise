60b721508e0f5fd46754257b197da86d44451029
zayed opu <zayedopu4933@gmail.com>
2024-09-12 22:02:36 +0600

    input text reciepe name and category set

--- Food-Paradise-Client/src/Pages/Dashboard/AddItems/AddItems.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,12 +1,49 @@
+import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
-
 const AddItems = () => {
-    return (
-        <div>
-            <SectionTitle heading={'add an item'} subHeading={"what's new"}></SectionTitle>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <SectionTitle
+        heading={"add an item"}
+        subHeading={"what's new"}
+      ></SectionTitle>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Reciepe Name*</span>
+            </label>
+            <input
+              type="text"
+              {...register("name")}
+              placeholder="Reciepe Name"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <select
+            {...register("category")}
+            className="select select-bordered w-full max-w-xs"
+          >
+            <option disabled selected>
+              Select a Category
+            </option>
+            <option value="salad">Salad</option>
+            <option value="pizza">Pizza</option>
+            <option value="soup">Soup</option>
+            <option value="dessert">Dessert</option>
+            <option value="drinks">Drinks</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
