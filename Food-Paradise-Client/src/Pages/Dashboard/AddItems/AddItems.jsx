b2a3a7da882bf2100052bf19e25b8ab2cae49c43
zayed opu <zayedopu4933@gmail.com>
2024-09-12 22:55:10 +0600

    add item form make

--- Food-Paradise-Client/src/Pages/Dashboard/AddItems/AddItems.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
@@ -7,58 +8,71 @@ const AddItems = () => {
     console.log(data);
   };
   return (
-    <div>
+    <div className="mx-24">
       <SectionTitle
         heading={"add an item"}
         subHeading={"what's new"}
       ></SectionTitle>
-      <div>
+      <div >
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-control w-full my-6">
+          <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Reciepe Name*</span>
             </label>
             <input
               type="text"
-              {...register("name")}
-              placeholder="Reciepe Name"
+              {...register("name", { required: true})}
+              placeholder="Recipe Name"
               className="input input-bordered w-full"
             />
           </div>
-          <div className="flex gap-6"> 
+          <div className="flex gap-6">
             {/* category */}
-            <div className="form-control w-full my-6">
-            <label className="label">
-              <span className="label-text">Category*</span>
-            </label>
-            <select
-            {...register("category")}
-            className="select select-bordered w-full"
-          >
-            <option disabled selected>
-              Select a Category
-            </option>
-            <option value="salad">Salad</option>
-            <option value="pizza">Pizza</option>
-            <option value="soup">Soup</option>
-            <option value="dessert">Dessert</option>
-            <option value="drinks">Drinks</option>
-          </select>
-          </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Category*</span>
+              </label>
+              <select
+                {...register("category", { required: true})}
+                className="select select-bordered w-full"
+              >
+                <option disabled selected>
+                  Select a Category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </div>
             {/* price */}
-            <div className="form-control w-full my-6">
-            <label className="label">
-              <span className="label-text">Price*</span>
-            </label>
-            <input
-              type="number"
-              {...register("price")}
-              placeholder="price"
-              className="input input-bordered w-full"
-            />
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Price*</span>
+              </label>
+              <input
+                type="number"
+                {...register("price", { required: true})}
+                placeholder="price"
+                className="input input-bordered w-full"
+              />
+            </div>
           </div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Recipe Details*</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"
+              {...register("recipe", { required: true})}
+            ></textarea>
+          </label>
+          <div className="form-control">
+          <input type="file" {...register("image", { required: true})} className="file-input w-full max-w-xs" />
           </div>
-          <input type="submit" />
+          <button className="btn btn-outline btn-accent w-full">ADD ITEMS <FaUtensils></FaUtensils></button>
         </form>
       </div>
     </div>
