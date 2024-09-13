fcec9d51edb445ae3ccb34ffac5656396ce3e01d
zayed opu <zayedopu4933@gmail.com>
2024-09-13 16:38:40 +0600

    table setup done

--- Food-Paradise-Client/src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,6 +1,14 @@
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 
 const ManageItems = () => {
+    const [menu] = useMenu();
+
+    const handleDeleteItem = item => {
+
+    }
+
   return (
     <div>
       <SectionTitle
@@ -25,37 +33,47 @@ const ManageItems = () => {
             </thead>
             <tbody>
               {
-
-              }
-              <tr>
-                <td>
-                  
-                </td>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                          alt="Avatar Tailwind CSS Component"
-                        />
+                menu.map((item, index) => <tr key={item._id}>
+                    <td>
+                      {index + 1}
+                    </td>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img
+                              src={item.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  
-                </td>
-                <td>
-
-                </td>
-                <td>
-                  <button className="btn btn-ghost btn-xs">details</button>
-                </td>
-                <td>
-
-                </td>
-              </tr>
+                    </td>
+                    <td>
+                      {item.name}
+                    </td>
+                    <td>
+                        ${item.price}
+                    </td>
+                    <td>
+                      <button
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn bg-orange-500 btn-sm"
+                  >
+                    <FaEdit className="text-white"></FaEdit>
+                  </button>
+                    </td>
+                    <td>
+                    <button
+                    onClick={() => handleDeleteItem(item)}
+                    className="btn btn-ghost btn-lg text-red-500"
+                  >
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                    </td>
+                  </tr>)
+              }
+              
             </tbody>
           </table>
         </div>
