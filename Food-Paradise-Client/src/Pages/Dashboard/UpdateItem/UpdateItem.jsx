df68fd46a7175dfd9314accf89851661f2baa903
zayed opu <zayedopu4933@gmail.com>
2024-09-14 22:26:49 +0600

    section title added for updateItem component

---
+++ Food-Paradise-Client/src/Pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+const UpdateItem = () => {
+    return (
+        <div>
+            <SectionTitle heading={"Update Item"} subHeading={"Refresh-info"}></SectionTitle>
+        </div>
+    );
+};
+
+export default UpdateItem;
