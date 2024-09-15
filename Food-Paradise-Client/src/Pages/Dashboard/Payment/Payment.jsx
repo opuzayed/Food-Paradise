584611cbdaa18d3eb68a0f1f1a629ea2d8893cc3
zayed opu <zayedopu4933@gmail.com>
2024-09-15 13:04:39 +0600

    stripe function impliment

--- Food-Paradise-Client/src/Pages/Dashboard/Payment/Payment.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
 
+const stripePromise = loadStripe("");
 
 const Payment = () => {
     return (
         <div>
         <SectionTitle heading={"Payment"} subHeading={"Pay to Eat"}></SectionTitle>
+        <div>
+            <Elements stripe = {stripePromise}>
+
+            </Elements>
+        </div>
         </div>
     );
 };
