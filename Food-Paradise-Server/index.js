a23c9253076655c8fccfbdfdcd79615fa5a41d15
zayed opu <zayedopu4933@gmail.com>
2024-09-15 22:29:50 +0600

    payment post api created

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -189,6 +189,20 @@ async function run() {
       res.send(result);
     });
 
+    //payment Intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency : "usd",
+        payment_method_types : ['card', 'link']
+      });
+      res.send({
+        clientSecret : paymentIntent.client_secret,
+      });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
