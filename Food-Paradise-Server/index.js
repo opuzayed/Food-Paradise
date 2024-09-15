a48ba037cd6bdc0fc62939c7107ea1dd637335ba
zayed opu <zayedopu4933@gmail.com>
2024-09-15 21:57:39 +0600

    require payment secret key

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')('process.env.STRIPE_SECRET_KEY');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -153,7 +154,6 @@ async function run() {
     }
     const result = await menuCollection.updateOne(filter, updateDoc);
     res.send(result);
-
     });
 
     app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
