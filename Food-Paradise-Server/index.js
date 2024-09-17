3d55618378ebf4eb5b9b47aafcc2ef578531baef
zayed opu <zayedopu4933@gmail.com>
2024-09-17 21:42:40 +0600

    get paymenthistory api created

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -206,7 +206,26 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
-      
+    });
+
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = {email: req.params.email};
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      console.log('payment info', payment);
+      const paymentResult = await paymentCollection.insertOne(payment);
+      const query = {_id : {
+        $in : payment.cartIds.map(id => new ObjectId(id))
+      }};
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({paymentResult, deleteResult});
     });
 
     // Send a ping to confirm a successful connection
