eed3af9fe9a3380be0271ec277d3efc43ed6febc
zayed opu <zayedopu4933@gmail.com>
2024-09-19 19:15:10 +0600

    conditionally show user name

--- Food-Paradise-Client/src/Pages/Dashboard/AdminHome/AsminHome.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/AdminHome/AsminHome.jsx
@@ -1,9 +1,16 @@
+import useAuth from "../../../hooks/useAuth";
 
 
 const AsminHome = () => {
+    const {user} = useAuth();
     return (
         <div>
-            
+            <h2 className="text-2xl">
+                <span>Hi! Welcome </span>
+                {
+                    user?.displayName ? user.displayName : 'Back'
+                }
+            </h2>
         </div>
     );
 };
