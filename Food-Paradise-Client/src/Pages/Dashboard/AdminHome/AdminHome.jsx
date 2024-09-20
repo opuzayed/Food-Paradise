df4b159b248893e871a18c4e2537a1f1492cbf69
zayed opu <zayedopu4933@gmail.com>
2024-09-20 21:11:57 +0600

    error solveld set loading before data fetching data

--- Food-Paradise-Client/src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,28 +1,80 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaDollarSign } from "react-icons/fa";
 
 const AsminHome = () => {
-    const {user} = useAuth();
-    const axiosSecure = useAxiosSecure();
-    const {data: stats} = useQuery({
-        queryKey: ['admin-stats'],
-        queryFn: async () => {
-            const res = await axiosSecure.get('/admin-stats');
-            return res.data;
-        }
-    })
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats, isLoading } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      console.log("revenue", res.data);
+      return res.data;
+    },
+  });
 
+  if(isLoading){
     return (
-        <div>
-            <h2 className="text-2xl font-bold">
-                <span>Hi! Welcome </span>
-                {
-                    user?.displayName ? user.displayName : 'Back'
-                }
-            </h2>
+        <div className="flex justify-center items-center h-screen">
+          <span className="loading loading-ring loading-lg"></span>
         </div>
-    );
+      );
+}
+
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold font-serif text-cyan-500">
+        <span>Hi! Welcome </span>
+        {user?.displayName ? user.displayName : "Back"}
+      </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-primary">
+            <FaDollarSign className="text-6xl font-bold"></FaDollarSign>
+          </div>
+          <div className="stat-title">REVENUE</div>
+          <div className="stat-value text-primary">{stats ? `$${stats.revenue}` : "Loading..."}</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 10V3L4 14h7v7l9-11h-7z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Page Views</div>
+          <div className="stat-value text-secondary">2.6M</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <div className="avatar online">
+              <div className="w-16 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+          </div>
+          <div className="stat-value">86%</div>
+          <div className="stat-title">Tasks done</div>
+          <div className="stat-desc text-secondary">31 tasks remaining</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AsminHome;
