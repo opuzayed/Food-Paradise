2c2a2ced5cda7f6c8c56d494dc6a7be4c818eb6d
zayed opu <zayedopu4933@gmail.com>
2024-09-22 21:29:15 +0600

    oreder-stats api created

--- Food-Paradise-Server/index.js
+++ Food-Paradise-Server/index.js
@@ -248,6 +248,11 @@ async function run() {
       res.send({users, menuItems, orders, revenue})
     });
 
+    //using aggregate-pipeline
+    app.get("/order-stats", async (req, res) => {
+      
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
