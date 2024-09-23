d910c958856f2959187e013e529b2b3995ccca92
zayed opu <zayedopu4933@gmail.com>
2024-09-23 13:18:41 +0600

    chartdata done

--- Food-Paradise-Client/src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ Food-Paradise-Client/src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,13 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { FaDollarSign, FaListAlt, FaShoppingCart, FaUsers } from "react-icons/fa";
+import {
+  FaDollarSign,
+  FaListAlt,
+  FaShoppingCart,
+  FaUsers,
+} from "react-icons/fa";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 
 const AsminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: stats, isLoading } = useQuery({
+  const { data: stats = {} } = useQuery({
     queryKey: ["admin-stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
@@ -16,22 +36,69 @@ const AsminHome = () => {
     },
   });
 
-  const {data: chartData} = useQuery({
-          queryKey: ['order-stats'],
-          queryFn: async () => {
-            const res = await axiosSecure.get('/order-stats');
-            return res.data;
-          }
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["order-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/order-stats");
+      return res.data;
+    },
   });
 
-  if(isLoading){
-    return (
-        <div className="flex justify-center items-center h-screen">
-          <span className="loading loading-ring loading-lg"></span>
-        </div>
-      );
-}
+  //custom shape for barChart
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
 
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  //custom shape for Pie-Chart
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const pieChartData = chartData.map(data => {
+    return {name: data.category, value: data.revenue}
+  });
+
+  //   if(isLoading){
+  //     return (
+  //         <div className="flex justify-center items-center h-screen">
+  //           <span className="loading loading-ring loading-lg"></span>
+  //         </div>
+  //       );
+  // }
 
   return (
     <div>
@@ -45,7 +112,7 @@ const AsminHome = () => {
             <FaDollarSign className="text-4xl font-bold"></FaDollarSign>
           </div>
           <div className="stat-title">REVENUE</div>
-          <div className="stat-value text-primary">{stats ? `$${stats.revenue}` : "Loading..."}</div>
+          <div className="stat-value text-primary">${stats.revenue} </div>
         </div>
 
         <div className="stat">
@@ -66,10 +133,63 @@ const AsminHome = () => {
 
         <div className="stat">
           <div className="stat-figure text-cyan-500">
-          <FaShoppingCart className="text-4xl font-bold"></FaShoppingCart>
+            <FaShoppingCart className="text-4xl font-bold"></FaShoppingCart>
           </div>
           <div className="stat-title">ORDERS</div>
           <div className="stat-value text-cyan-500">{stats.orders}</div>
+        </div>
+      </div>
+      <div className="flex my-10">
+        <div className="w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        <div className="w-1/2">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+            <Legend></Legend>
+              <Pie
+                data={pieChartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {pieChartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
