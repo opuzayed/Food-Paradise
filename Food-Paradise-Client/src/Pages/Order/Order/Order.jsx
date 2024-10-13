c008590b7723a4cbe760934e536f791a263bf6be
zayed opu <zayedopu4933@gmail.com>
2024-10-13 15:06:37 +0600

    margin bottom added to tabitem

--- Food-Paradise-Client/src/Pages/Order/Order/Order.jsx
+++ Food-Paradise-Client/src/Pages/Order/Order/Order.jsx
@@ -39,7 +39,7 @@ const Order = () => {
             <Cover img={orderImg} title={"Order Food"} shortDes={"Would You Like To Try Our Dish? Order Your Favourite Food, which is available right now, also enjoy discount for every item."} className="mb-20"></Cover>
             <div className='mb-20'>
                 <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-                    <TabList className='text-center font-bold text-2xl text-sky-600'>
+                    <TabList className='text-center font-bold text-2xl text-sky-600 mb-10'>
                         <Tab>Salad</Tab>
                         <Tab>Pizza</Tab>
                         <Tab>Soup</Tab>
